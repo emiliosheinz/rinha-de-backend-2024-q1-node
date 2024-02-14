5ef1a5510ad0f48852e79eab960ad0be42f47c32
Fumegalli <rafael.fumegalli@cwi.com.br>
2024-02-14 10:53:39 -0300

    feat: add request validation config

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,18 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.API_PORT, process.env.API_HOST);
-  console.log('🚀 API running on ', `http://${process.env.API_HOST}:${process.env.API_PORT}`);
+  console.log(
+    '🚀 API running on ',
+    `http://${process.env.API_HOST}:${process.env.API_PORT}`,
+  );
 }
 bootstrap();
