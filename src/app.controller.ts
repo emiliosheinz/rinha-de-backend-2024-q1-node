6db78cb29ad1ac056fa0fd882e6a9833b88aac4c
Fumegalli <rafael.fumegalli@cwi.com.br>
2024-02-14 10:55:08 -0300

    feat: add request body and param validation to the create transaction endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateTransactionDto } from './dtos/create-transaction.dto';
 
@@ -8,6 +15,11 @@ export class AppController {
 
   @Post('/:id/transacoes')
   createTransaction(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
     @Body() createTransactionDto: CreateTransactionDto,
   ): Promise<string> {
     console.log(createTransactionDto);
--- src/dtos/create-transaction.dto.ts
+++ src/dtos/create-transaction.dto.ts
@@ -1,7 +1,11 @@
+import { IsEnum, IsInt, Length } from 'class-validator';
 import { TransactionType } from 'src/entities/transaction.entity';
 
 export class CreateTransactionDto {
+  @IsInt()
   valor: number;
+  @IsEnum(TransactionType)
   tipo: TransactionType;
+  @Length(1, 10)
   descricao: string;
 }
