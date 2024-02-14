f7a9a7bc683f3e4d02e4006e4d6cb30f9c9ddf25
Fumegalli <rafael.fumegalli@cwi.com.br>
2024-02-14 11:29:48 -0300

    feat: change http code to be 200

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  HttpCode,
   HttpStatus,
   Param,
   ParseIntPipe,
@@ -14,6 +15,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('/:id/transacoes')
+  @HttpCode(200)
   createTransaction(
     @Param(
       'id',
