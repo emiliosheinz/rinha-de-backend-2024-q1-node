8eafdc4416b602a561710ad5d6c711d96a2ad65c
Emilio Schaedler Heinzmann <emilio.heinzmann@cloudbeds.com>
2024-02-14 10:17:58 -0300

    fix: containers issue

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(process.env.API_PORT);
+  await app.listen(process.env.API_PORT, '0.0.0.0');
   console.log('🚀 API running on port', process.env.API_PORT);
 }
 bootstrap();
