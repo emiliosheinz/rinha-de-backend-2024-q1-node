969c25f2ec48a11adefba0f62005de4cb88c57d9
Fumegalli <rafael.fumegalli@cwi.com.br>
2024-02-15 23:44:23 -0300

    test: add e2e tests for createTransaction endpoint

--- src/dtos/create-transaction.dto.ts
+++ src/dtos/create-transaction.dto.ts
@@ -1,8 +1,9 @@
-import { IsEnum, IsInt, Length } from 'class-validator';
+import { IsEnum, IsInt, IsPositive, Length } from 'class-validator';
 import { TransactionType } from 'src/transaction/transaction.entity';
 
 export class CreateTransactionDto {
   @IsInt()
+  @IsPositive()
   valor: number;
   @IsEnum(TransactionType)
   tipo: TransactionType;
