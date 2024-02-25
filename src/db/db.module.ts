d02d3af296c4dfee5436f01fd93f8ea8a8343c69
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2024-02-25 12:24:28 -0300

    chore: add log to db module

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -3,6 +3,15 @@ import { Pool } from 'pg';
 import { DATABASE_CONNECTION } from './db.constants';
 import { DbService } from './db.service';
 
+console.log({
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOSTNAME,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+  max: 12,
+});
+
 const dbProvider: Provider = {
   provide: DATABASE_CONNECTION,
   useValue: new Pool({
