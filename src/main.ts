6d91b7596e95550c4ed6c2aafb4a92395530bfdc
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2024-02-25 12:00:38 -0300

    feat: add logs

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: false }),
-    { logger: false },
+    new FastifyAdapter({ logger: true }),
+    { logger: console },
   );
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
   await app.listen(process.env.API_PORT, process.env.API_HOST);
